0fcaee771d551844c31fc9f368433bf6cc6e984f
Thompson Plyler <nealok.g@gmail.com>
2023-05-10 13:47:31 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("I am connected!")
+const button = document.querySelector("#example")
+const form = 
+console.log(button)
+
+const clickHandler = () => {
+    console.log("click")
+}
+button.addEventListener("click", clickHandler)
